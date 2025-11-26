e0825f2fb35bb7dbe5823964543eea563ea97a43
Krokox <andyalv21@gmail.com>
2025-11-26 10:23:27 -0300

    Contadores del QuickSort Terminados

--- visualizador/algorithms/sort_quick.py
+++ visualizador/algorithms/sort_quick.py
@@ -7,39 +7,33 @@ inicio = None
 fin = None
 i = None
 j = None   
-# Agregá acá tus punteros/estado, p.ej.:
-# i = 0; j = 0; fase = "x"; 
 stack = []
-
-
+comparison_count = 0 
+swap_count = 0    
+# Defino las variables
 def init(vals):
-    global items,n,inicio,fin,i,j,stack
-    items = list(vals)
-    n = len(items)
-    inicio = 0
-    fin = n-1
-    i = None
-    j = inicio
+    global items,n,inicio,fin,i,j,stack,comparison_count, swap_count
+    items = list(vals) #la lista
+    n = len(items) # la longitud de la lista
+    inicio = 0 #
+    fin = n-1 #
+    i = None #
+    j = inicio #
     # TODO: inicializar punteros/estado
     stack.append([inicio, fin])
 
 def step():
-    global items, n, inicio, fin, i, j, stack
+    global items, n, inicio, fin, i, j, stack, comparison_count, swap_count
     
     if len(stack) == 0:
-        return {"done": True}
-    #algoritmo quick sort 
+        return {"done": True, "comp_count": comparison_count,"swap_count": swap_count}
     inicioAFin = stack.pop()
-    # 1- Agarro el ultimo
-    
-    #pivot_val = items[fin] ← no lo mezcles con el índice guardado
-    
+       
+    comparison_count +=1
     current = inicioAFin[0] + j
     pivot = inicioAFin[1]
 
     if current < pivot:
-    #2- Recorro desde el primero hasta el ante ultimo
-    #items[fin] va a ser pivot_val
         if items[current] > items[pivot]:
             if i == None:
                 i = current
@@ -47,11 +41,7 @@ def step():
                 j=j+1
             stack.append(inicioAFin) #
             print(pivot, current)
-            return {"a": pivot, "b": current, "swap": False, "done": False}
-        #j va a recorrer de inicio a fin-1 
-        #si items[j] > pivot_val
-        #si i = none, guardo i = j 
-        #si i ya tiene valor, no hago nada (avanzo j)
+            return {"a": pivot, "b": current, "swap": False, "done": False, "comp_count": comparison_count,"swap_count": swap_count}
 
         if items[current] < items[pivot]:
             if i == None:
@@ -62,15 +52,9 @@ def step():
                 i = i+1
         
                 stack.append(inicioAFin)
-                return {"a": temp, "b": current, "swap": True, "done": False}
-    #si items[j] <= pivot_val
-    #si i es none, no hago nada (avanzo j)
-	#si i tiene un valor, hago swap items[i], items[j], guardado y avanzo i en uno, avanzo j en uno
-
-    #3- al terminar de pasar:
-    #si i es none, no hago swap al final, el pivot ya esta en el final
-    #si i no es none, hago swap(items[i], items[fin] para poner el pivot en i
-    
+                swap_count +=1
+                return {"a": temp, "b": current, "swap": True, "done": False, "comp_count": comparison_count,"swap_count": swap_count}
+  
         stack.append(inicioAFin)
     else:
         if i != None:
@@ -86,9 +70,10 @@ def step():
             #reinicio las variables
             j = 0
             i = None
-            return {"a": temp, "b": current, "swap": True, "done": False}
+            swap_count +=1
+            return {"a": temp, "b": current, "swap": True, "done": False,"comp_count": comparison_count,"swap_count": swap_count}
         else:
             if (inicioAFin[0] < inicioAFin[1]-1):
                 stack.append([inicioAFin[0], inicioAFin[1]-1])
             j = 0
-    return {"done": False}
+    return {"done": False, "comp_count": comparison_count,"swap_count": swap_count}
