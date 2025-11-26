f04403159ae551deff14aee84ef02f5239cfca75
Krokox <andyalv21@gmail.com>
2025-11-26 10:48:46 -0300

    Contadores del Selection Terminados

--- visualizador/algorithms/sort_selection.py
+++ visualizador/algorithms/sort_selection.py
@@ -6,6 +6,8 @@ i = 0          # cabeza de la parte no ordenada
 j = 0          # cursor que recorre y busca el mínimo
 min_idx = 0    # índice del mínimo de la pasada actual
 fase = "buscar"  # "buscar" | "swap"
+comparison_count = 0 
+swap_count = 0  
 
 def init(vals):
     global items, n, i, j, min_idx, fase
@@ -17,15 +19,16 @@ def init(vals):
     fase = "buscar"
 
 def step():
-    global items, n, i, j, min_idx, fase
+    global items, n, i, j, min_idx, fase,comparison_count, swap_count 
     # - Fase "buscar": comparar j con min_idx, actualizar min_idx, avanzar j.
     if fase == "buscar":
+        comparison_count +=1
         if  j < n:  
             if items[j] < items[min_idx]:
                 min_idx = j
             j = j+1
     #   Devolver {"a": min_idx, "b": j_actual, "swap": False, "done": False}.
-            return {"a": min_idx, "b": j, "swap": False, "done": False}
+            return {"a": min_idx, "b": j, "swap": False, "done": False, "comp_count": comparison_count,"swap_count": swap_count}
     #   Al terminar el barrido, pasar a fase "swap".
     
     fase = "swap"
@@ -33,11 +36,12 @@ def step():
     if fase == "swap":
         # if min_idx != i:
             if items[i] > items[min_idx]:
+                swap_count +=1
                 # aux = items[min_idx]
                 # items[min_idx] = items[i]
                 # items[i] = aux
                 items[min_idx], items[i] = items[i], items[min_idx]
-                return {"a": i, "b": min_idx, "swap": True, "done": False}
+                return {"a": i, "b": min_idx, "swap": True, "done": False, "comp_count": comparison_count,"swap_count": swap_count}
     # - Fase "swap": si min_idx != i, hacer ese único swap y devolverlo.
     #   Luego avanzar i, reiniciar j=i+1 y min_idx=i, volver a "buscar".
     i = i+1
@@ -45,8 +49,8 @@ def step():
     min_idx = i
     fase = "buscar"
     if i < n:
-        return {"a": min_idx, "b": j, "swap": False, "done": False}
+        return {"a": min_idx, "b": j, "swap": False, "done": False, "comp_count": comparison_count,"swap_count": swap_count}
     #
     # Cuando i llegue al final, devolvé {"done": True}.
     
-    return {"done": True}
+    return {"done": True, "comp_count": comparison_count,"swap_count": swap_count}
